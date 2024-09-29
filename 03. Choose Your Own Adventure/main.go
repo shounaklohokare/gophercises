a3759f148f96dbf8345bbf6a89a3638eabc6513a
Shounak Lohokare <shounaklohokare@gmail.com>
2024-09-29 20:23:18 +0530

    added CLI option to play cyoa game

--- 03. Choose Your Own Adventure/main.go
+++ 03. Choose Your Own Adventure/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -25,18 +26,57 @@ type Story map[string]Chapter
 
 func main() {
 
-	filename := flag.String("filename", "gopher.json", "path to the JSON file containing the story")
+	filename := flag.String("filename", "gopher.json", "Path to the JSON file containing the story")
+	webserver := flag.Bool("use_webserver", true, "Pass true to enable the game on the web server, or false to use the CLI.")
 	flag.Parse()
 
 	var story Story
 
 	parseFile(filename, &story)
 
+	if *webserver {
+		// use web server
+		useWebserver(story)
+	} else {
+		// use CLI
+		useCLI(story)
+	}
+
+}
+
+func useCLI(story Story) {
+
+	nextArc := "intro"
+
+	var op int
+	for {
+
+		arc := story[nextArc]
+
+		fmt.Println(arc.Title + "\n")
+		fmt.Println(arc.Paragraphs[0] + "\n")
+
+		if len(arc.Options) == 0 {
+			break
+		}
+
+		fmt.Printf("Please choose one of the following options to proceed:- ")
+		for i, option := range arc.Options {
+			fmt.Printf("\n%v. %v", (i + 1), option.Text)
+		}
+
+		fmt.Scanln(&op)
+
+		nextArc = arc.Options[op-1].Chapter
+
+	}
+}
+
+func useWebserver(story Story) {
 	http.HandleFunc("/", story.handler)
 	http.HandleFunc("/{arc}", story.handleArc)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
-
 }
 
 func (story Story) handler(w http.ResponseWriter, r *http.Request) {
