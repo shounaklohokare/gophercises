48bbe82875043fc41b101d19218eb42f9bcda53e
Shounak Lohokare <shounaklohokare@gmail.com>
2024-10-06 22:54:42 +0530

    Completed link parser excercise

--- 04. HTML Link Parser/main.go
+++ 04. HTML Link Parser/main.go
@@ -4,9 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
+
+type link struct {
+	Text string
+	Href string
+}
 
 func main() {
 
@@ -24,5 +30,58 @@ func main() {
 	if err != nil {
 		fmt.Printf("Failed to parse the HTML file:- %s", err)
 	}
+
+	aChan := make(chan *html.Node)
+	go findAnchors(root, aChan)
+
+	for a := range aChan {
+		fmt.Printf("%+v\n", link{
+			Text: getText(a),
+			Href: getHref(a),
+		})
+	}
+
+}
+
+func findAnchors(node *html.Node, aChan chan *html.Node) {
+
+	if node.Type == html.ElementNode && node.Data == "a" {
+		aChan <- node
+		return
+	}
+
+	for next := node.FirstChild; next != nil; next = next.NextSibling {
+		findAnchors(next, aChan)
+	}
+
+	if node.Parent == nil {
+		close(aChan)
+	}
+
+}
+
+func getHref(node *html.Node) string {
+
+	for _, attr := range node.Attr {
+		if attr.Key == "href" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func getText(node *html.Node) string {
+
+	var text string
+
+	for next := node.FirstChild; next != nil; next = next.NextSibling {
+		if next.Type == html.TextNode {
+			text += next.Data
+		} else {
+			text += getText(next)
+		}
+	}
+
+	return strings.TrimSpace(text)
 
 }
